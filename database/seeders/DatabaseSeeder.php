68cf2514920832dd5b38787a7e8698ad04cd281d
diannugraheni82 <bernadethadian5@gmail.com>
2026-01-05 20:07:26 +0700

    admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,23 +6,39 @@ use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
 class DatabaseSeeder extends Seeder
 {
     use WithoutModelEvents;
 
-   public function run(): void
-{
-    $this->call([
-        KategoriUmkmSeeder::class,
-        LokasiSeeder::class,
-        PenggunaSeeder::class, 
-        UmkmSeeder::class,
-        LegalitasUmkmSeeder::class,
-        PembayaranPendaftaranSeeder::class,
-        EventSeeder::class,             
-        PendaftaranEventSeeder::class,  
-        NotifikasiSeeder::class,
-        BeritaSeeder::class,
-    ]);
-}
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('admin123'),
+            'role' => 'admin',
+        ]);
+
+        $this->call([
+            KategoriUmkmSeeder::class,
+            LokasiSeeder::class,
+            PenggunaSeeder::class, 
+            UmkmSeeder::class,
+            LegalitasUmkmSeeder::class,
+            PembayaranPendaftaranSeeder::class,
+            EventSeeder::class,             
+            PendaftaranEventSeeder::class,  
+            NotifikasiSeeder::class,
+            BeritaSeeder::class,
+        ]);
+    }
 }
